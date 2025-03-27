dcc9e511bc446119f53598cbd22e0da2defc9f27
Vadim <_s_e_t__@ukr.net>
2025-03-27 18:26:22 +0200

    create a new node REST API app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listining on port ${port}`);
+});
