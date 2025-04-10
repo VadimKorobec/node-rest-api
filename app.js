cf227a397a2dcd485d878304a8c5e50437ee7572
VadimKorobec <_s_e_t__@ukr.net>
2025-04-10 15:46:43 +0300

    add some code

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -9,6 +10,10 @@ app.use(cors());
 const postsRouter = require("./routers/posts");
 
 app.use("/api/posts", postsRouter);
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
+});
 
 // pdJ5TNoSHi61rxWA
 
--- server.js
+++ server.js
@@ -2,18 +2,13 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const port = 8080;
-
-const DB_HOST =
-  "mongodb+srv://Vadim:pdJ5TNoSHi61rxWA@cluster0.g5vrjd2.mongodb.net/posts_reader?retryWrites=true&w=majority&appName=Cluster0";
-
 mongoose
-  .connect(DB_HOST)
+  .connect(process.env.DB_HOST)
   .then(() => {
-    app.listen(port);
-    console.log(`Database connect success port ${port}`);
+    app.listen(process.env.PORT);
+    console.log("Database connected successfully");
   })
   .catch((error) => {
     console.log(error.message);
-    process.exit(1)
+    process.exit(1);
   });
