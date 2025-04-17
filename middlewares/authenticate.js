cd811dbf939a5d144201a0a6875b44037d4f3ef8
Vadim <_s_e_t__@ukr.net>
2025-04-17 17:55:51 +0300

    add authenticate.js

---
+++ middlewares/authenticate.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const { User } = require("../models/user");
+
+const HttpError = require("../helpers/HttpError");
+
+const authenticate = async (req, res, next) => {
+  const { authorization = "" } = req.headers;
+  console.log(authorization);
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer") {
+    next(HttpError(401));
+  }
+  try {
+    const { id } = jwt.verify(token, process.env.SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user) {
+      next(HttpError(401));
+    }
+    next();
+  } catch {
+    next(HttpError(401));
+  }
+};
+
+module.exports = authenticate;
--- routers/posts.js
+++ routers/posts.js
@@ -6,24 +6,32 @@ const ctrl = require("../controllers/posts");
 
 const validateBody = require("../middlewares/validateBody");
 const isValidId = require("../middlewares/isValidId");
+const authenticate = require("../middlewares/authenticate");
 
 const { schemas } = require("../models/post");
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:id", isValidId, ctrl.getById);
+router.get("/:id", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.addPost);
+router.post("/", authenticate, validateBody(schemas.addSchema), ctrl.addPost);
 
-router.put("/:id", isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+router.put(
+  "/:id",
+  authenticate,
+  isValidId,
+  validateBody(schemas.addSchema),
+  ctrl.updateById
+);
 
 router.patch(
   "/:id/favorite",
+  authenticate,
   isValidId,
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite
 );
 
-router.delete("/:id", isValidId, ctrl.deleteById);
+router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
 
 module.exports = router;
